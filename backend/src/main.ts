728e2a5a56fdbb4e2964f3269801350679e0fd08
whiteehat <150809795+whiteehat@users.noreply.github.com>
2026-03-17 00:42:21 +0100

    Modify CORS and port configuration in main.ts
    Updated CORS settings to allow specific origin and added support for dynamic port from environment variables.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,15 @@
-﻿import { NestFactory } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  app.enableCors();
-  await app.listen(3000);
+  app.enableCors({
+    origin: ['https://veh-auct.vercel.app'], // no trailing slash
+    credentials: true,
+  });
+  
+  await app.listen(process.env.PORT || 3000); // use Render's PORT
 }
 bootstrap();
